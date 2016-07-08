26ee314c4dd26e2e2ec8e29ba142f12de5dc1485
Nick <nbdegroot1@gmail.com>
2016-07-07 18:41:26 -0700

    Rain Updating
    - Checks if it's still raining every 10 minutes.
    - Created checkHumidity() for (you guessed it) humidity checking and
    setting the isRaining variable.

--- Automatic-Cover/Automatic-Cover.ino
+++ Automatic-Cover/Automatic-Cover.ino
@@ -3,12 +3,12 @@ THE AUTOMATIC COVER - SENSOR EDITION
 
 Ports:
 Pressure - Analog pin A0
-Humidity - Digital pin 8 (3.3v)
+Humidity (DHT22) - Digital pin 8 (3.3v)
 LCD - Digital pins 2, 3, 4, 5, 11, and 12
-*/
 
+NEEDED LIBARIES:
+DHT Sensor Libary: https://github.com/adafruit/DHT-sensor-library
 
-/*
 TO-DO
 
 1. Check if it's still raining every 10 minutes
@@ -28,6 +28,49 @@ LiquidCrystal lcd(12,11,5,4,3,2);
 bool isRaining;
 int pressure;
 float humidity;
+unsigned long previousMillis = 0;
+const long interval = 60000; // Rain update frequency (ms)
+
+void checkHumidity() {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Checking");
+  lcd.setCursor(0,1);
+  lcd.print("humidity");
+  delay(3000);
+  float humidity = dht.readHumidity();
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println("%");
+
+  if (humidity > 50) {
+
+  // IF PRESSURE DETECTED AND HUMIDITY > 90%, isRaining = true
+
+    bool isRaining = true;
+    Serial.print("Rain Detected");
+    Serial.println("");
+    lcd.clear();
+    lcd.print("Rain");
+    lcd.setCursor(0,1);
+    lcd.print("detected");
+    delay(10000);
+  }
+  else {
+
+    // IF PRESSURE DETECTED AND HUMIDITY < 90%, isRaining = False
+
+    bool isRaining = false
+    Serial.print("False positive - humidity override");
+    Serial.println("");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Humidity");
+    lcd.setCursor(0,1);
+    lcd.print("Override");
+    delay(10000);
+    }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -60,41 +103,16 @@ void loop() {
   lcd.setCursor(0,1);
   lcd.print("detected");
   delay(1000);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Checking");
-  lcd.setCursor(0,1);
-  lcd.print("humidity");
-  delay(3000);
-  float humidity = dht.readHumidity();
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.println("%");
-  if (humidity > 50) {
-
- // IF PRESSURE DETECTED AND HUMIDITY > 90%, isRaining = true
-
-    bool isRaining = true;
-    Serial.print("Rain Detected");
-    Serial.println("");
-    lcd.clear();
-    lcd.print("Rain");
-    lcd.setCursor(0,1);
-    lcd.print("detected");
-    delay(10000);
+  checkHumidity();
   }
-  else {
-    Serial.print("False positive - humidity override");
-    Serial.println("");
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Humidity");
-    lcd.setCursor(0,1);
-    lcd.print("Override");
-    delay(10000);
+
+// RAIN UPDATING
+
+unsigned long currentMillis = millis();
+if (currentMillis - previousMillis >= interval) {
+  previousMillis = currentMillis;
+  if isRaining = true {
+    checkHumidity();
     }
   }
 }
-
-// Testing, Testing
-// One, Two, Three
